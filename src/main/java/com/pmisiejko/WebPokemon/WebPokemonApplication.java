e8b5534c22e80188fad626f1a6c5eababcaf0fed
pmisiejko <pmisiejko@gmail.com>
2020-04-24 18:34:49 +0200

    Start Project

---
+++ src/main/java/com/pmisiejko/WebPokemon/WebPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.pmisiejko.WebPokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmisiejko/WebPokemon/WebPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmisiejko.WebPokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
